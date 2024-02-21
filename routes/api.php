e297caea7ed61fdbd8da8dd684f9def425cfc6ae
usamatoor1993 <33315058+usamatoor1993@users.noreply.github.com>
2024-02-21 12:46:56 +0500

    orget password
    forget password implemented

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/signup', 'App\Http\Controllers\Api\AuthController@signup');
 Route::post('/login', 'App\Http\Controllers\Api\AuthController@login');
 Route::post('/forgot', 'App\Http\Controllers\Api\AuthController@forgot');
-Route::post('/password/reset', 'App\Http\Controllers\Api\AuthController@reset');
+Route::post('/password/reset', 'App\Http\Controllers\Api\AuthController@reset')->middleware('auth:sanctum');
 Route::get('/getUserById/{id}', 'App\Http\Controllers\Api\AuthController@getUserById')->middleware('auth:sanctum');
 Route::post('/reviewCompany', 'App\Http\Controllers\Api\GuestController@reviewCompany')->middleware('auth:sanctum');
+
 
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/clear', function() {
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('route:cache');
+    return "clear";
+});
+Route::get('/', 
+function () {
     return view('welcome');
 });
+Route::get('/forgot_password', function () {
+    return view('forget-password');
+})->name('password.reset');
