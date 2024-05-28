b56e3b1e9f52087d1ac051b7908523a86a0b7a61
atifkodex <110590569+atifkodex@users.noreply.github.com>
2024-05-28 14:25:18 +0500

    issue resove like company and review company

--- app/Http/Controllers/Api/ActivityController.php
+++ app/Http/Controllers/Api/ActivityController.php
@@ -751,10 +751,10 @@ class ActivityController extends Controller
         if ($validator->fails()) {
             return response(['status' => 'error', 'code' => 403, 'user' => null, 'data' => null, 'message' => $validator->errors()], 403);
         }
-        $company = Company::where('user_id', $request->userId)->first();
-        if (!$company) {
-            return response(['status' => 'error', 'code' => 403, 'user' => null, 'data' => null, 'message' => 'Company not found'], 403);
-        }
+        // $company = Company::where('user_id', $request->userId)->first();
+        // if (!$company) {
+        //     return response(['status' => 'error', 'code' => 403, 'user' => null, 'data' => null, 'message' => 'Company not found'], 403);
+        // }
         $company = Company::where('id', $request->id)->first();
 
         if ($company) {
--- app/Http/Controllers/Api/GuestController.php
+++ app/Http/Controllers/Api/GuestController.php
@@ -18,7 +18,7 @@ class GuestController extends Controller
         $validator = Validator::make($request->all(), [
             'companyId' => 'required',
             // 'comment' => 'required',
-            // 'stars' => 'required|numeric|min:1|max:5',
+            'stars' => 'required|numeric|min:1|max:5',
         ]);
         if ($validator->fails()) {
             return response(['status' => 'error', 'code' => 422, 'message' => 'missing or wrong params', 'errors' => $validator->errors()->all()], 422);
